9805b3209e2bdd3644f60e119da095a1f934a72a
Humaeid Bin Mansur <humaeidmansur@gmail.com>
2025-09-20 00:44:47 +0200

    added Loading function and Modal with java script code

--- script/index.js
+++ script/index.js
@@ -1,3 +1,17 @@
+const createElements =(arr) => {
+    const htmlElements= arr.map((el) => `<span class="btn">${el}</span>`);
+return htmlElements.join("");
+};
+const manageSpinner=(status)=>{
+    if(status==true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add("hidden");
+    } else {
+        document.getElementById("word-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+    }
+};
+
 const loadlessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
     .then((res)=> res.json()) // promise of json data
@@ -9,6 +23,7 @@ const removeActive = () => {
 }
 
 const loadLevelWord = (id) => {
+    manageSpinner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
         fetch(url)
         .then((res) => res.json())
@@ -30,9 +45,10 @@ if(words.length == 0){
     <div class="text-center col-span-full rounded-xl py-10 font-bangla space-y-6">
     <img src="./assets/alert-error.png" class="mx-auto"/>
     <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p> 
-    <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    <h2 class="font-bold text-4xl">নেক্সট Lesso n এ যান</h2>
     </div>
     `;
+    manageSpinner(false);
     return;
 }
 
@@ -53,14 +69,69 @@ words.forEach(word => {
     <p class="font-semibold">Meaning /Pronounciation</p>
     <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "There is no meaning"} / ${word.pronunciation? word.pronunciation : "No pronounciation" }"</div>
       <div class="flex justify-between items-center">
-    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+    <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
     <button class="btn bg-[#1A91FF10]  hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></i></button>
       </div>
     </div>
     `;
     wordContainer.append(card);
-})
+});
+manageSpinner(false);
 
+};
+
+
+// "status": true,
+// "message": "successfully fetched a word details",
+// "data": {
+// "word": "Eager",
+// "meaning": "আগ্রহী",
+// "pronunciation": "ইগার",
+// "level": 1,
+// "sentence": "The kids were eager to open their gifts.",
+// "points": 1,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "enthusiastic",
+// "excited",
+// "keen"
+// ],
+// "id": 5
+// }
+
+
+
+const loadWordDetail= async (id) => {
+const url =`https://openapi.programming-hero.com/api/word/${id}`;
+const res = await fetch(url);
+const details= await res.json();
+displayWordDetails(details.data);
+};
+
+
+
+const displayWordDetails = (word) => {
+const detailsBox=document.getElementById("details-container");
+detailsBox.innerHTML = `
+    <div class="">
+      <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})
+      </h2>
+    </div>
+    <div class="">
+      <h2 class="font-bold">Meaning </h2>
+      <p>${word.meaning}</p>
+    </div>
+    <div class="">
+      <h2 class="font-bold">Example </h2>
+      <p>${word.sentence}</p>
+    </div>
+    <div class="">
+      <h2 class="font-bold">Synonyms </h2>
+      <div class="">${createElements(word.synonyms)}</div>
+    </div>
+`;
+
+document.getElementById("my_modal_5").showModal();
 };
 
 const displayLesson = (lessons) => {
@@ -72,7 +143,7 @@ levelContainer.innerHTML = ``;
 for (let lesson of lessons) {
 
 //  3.create Element
-console.log(lesson);
+// console.log(lesson);
 
 const btnDiv = document.createElement("div");
 btnDiv.innerHTML = `  
