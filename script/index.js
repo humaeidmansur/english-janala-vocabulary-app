83b8ed0e9f373395f38a49296c321a9d2402d88a
Humaeid Bin Mansur <humaeidmansur@gmail.com>
2025-09-19 16:32:54 +0200

    added lesson four seven and some title in bottom with java script programme

--- script/index.js
+++ script/index.js
@@ -3,17 +3,38 @@ const loadlessons = () => {
     .then((res)=> res.json()) // promise of json data
     .then((json) => displayLesson(json.data));
 };
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-btn");
+    lessonButtons.forEach(btn=> btn.classList.remove("active"));
+}
 
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
         fetch(url)
         .then((res) => res.json())
-        .then((data) => displayLevelWord(data.data));
-}
+        .then((data) => {
+            removeActive(); //remove all active class
+            const clickBtn= document.getElementById(`lesson-btn-${id}`);
+            // console.log(clickBtn)
+            clickBtn.classList.add("active"); //add active class
+            displayLevelWord(data.data);
+});
+};
 
 const displayLevelWord=(words)=>{
 const wordContainer=document.getElementById("word-container");
 wordContainer.innerHTML="";
+
+if(words.length == 0){
+    wordContainer.innerHTML=`
+    <div class="text-center col-span-full rounded-xl py-10 font-bangla space-y-6">
+    <img src="./assets/alert-error.png" class="mx-auto"/>
+    <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p> 
+    <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    </div>
+    `;
+    return;
+}
 
 //  {
 //     "id": 82,
@@ -28,9 +49,9 @@ words.forEach(word => {
     const card=document.createElement("div");
     card.innerHTML=`  
     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-    <h2 class="font-bold text-2xl">${word.word}</h2>
+    <h2 class="font-bold text-2xl">${word.word ? word.word :"There is no Word"}</h2>
     <p class="font-semibold">Meaning /Pronounciation</p>
-    <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+    <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "There is no meaning"} / ${word.pronunciation? word.pronunciation : "No pronounciation" }"</div>
       <div class="flex justify-between items-center">
     <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
     <button class="btn bg-[#1A91FF10]  hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></i></button>
@@ -55,7 +76,7 @@ console.log(lesson);
 
 const btnDiv = document.createElement("div");
 btnDiv.innerHTML = `  
-<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+<button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
 <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
 </button> `;
 //  4.append into container
